25e357fd7dd0e1d9ac8c6d06b38c636c007570de
koralangel <75666378+koralangel@users.noreply.github.com>
2021-01-07 12:03:39 +0200

    Add files via upload

---
+++ build/precache-manifest.6abcdcb80084a2e9784d74a5875080d1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fc13a607c5afaa9d940ff7d4a33161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "834c7d3c7cea6767c681",
+    "url": "/static/css/2.3d666daf.chunk.css"
+  },
+  {
+    "revision": "4114c60c4fafb2c9deb4",
+    "url": "/static/css/main.a44fb032.chunk.css"
+  },
+  {
+    "revision": "834c7d3c7cea6767c681",
+    "url": "/static/js/2.f232fe77.chunk.js"
+  },
+  {
+    "revision": "4114c60c4fafb2c9deb4",
+    "url": "/static/js/main.f60c4ece.chunk.js"
+  },
+  {
+    "revision": "85d5f3d95529db479f20",
+    "url": "/static/js/runtime-main.e5e68f06.js"
+  },
+  {
+    "revision": "5dc62f4ef1e57a7c3d89ec4095d8a0ae",
+    "url": "/static/media/Adoption.5dc62f4e.png"
+  },
+  {
+    "revision": "53eaf0307e3b873528e4921bbb552358",
+    "url": "/static/media/Adoption2.53eaf030.png"
+  },
+  {
+    "revision": "4d5c06d02d885acb40de2e4ec8bd816b",
+    "url": "/static/media/amex.4d5c06d0.svg"
+  },
+  {
+    "revision": "ce2750b3223790b144d946597b7e0b5c",
+    "url": "/static/media/contect.ce2750b3.png"
+  },
+  {
+    "revision": "9e30b9f2e694a19a4e045261d9cc1e5b",
+    "url": "/static/media/diners.9e30b9f2.svg"
+  },
+  {
+    "revision": "ff8f8d752b05941dac629b298a493910",
+    "url": "/static/media/lo.ff8f8d75.png"
+  },
+  {
+    "revision": "04088ba376acede9cad745956a11d8dd",
+    "url": "/static/media/mastercard.04088ba3.svg"
+  },
+  {
+    "revision": "a53e1ecf8fc1126ecb2850dcfa85b1a9",
+    "url": "/static/media/petfoot.a53e1ecf.png"
+  },
+  {
+    "revision": "31c0aad35435a3528697ff37b7e18b84",
+    "url": "/static/media/security-code.31c0aad3.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "bca381c0504cc89620c47279f2d7c8d4",
+    "url": "/static/media/visa.bca381c0.svg"
+  }
+]);
